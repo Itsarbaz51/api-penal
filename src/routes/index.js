8e9c978c1b7074d68504b447e0d265bd79e7fd10
arbaz khan <itsarbaz51@gmail.com>
2026-05-11 13:52:26 +0530

    done provider

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ import authRoute from "./auth.route.js";
 import packageRoute from "./package.routes.js";
 import userRoute from "./user.routes.js";
 import serviceRoute from "./service.route.js";
-
+import providerRoute from "./provider.route.js";
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/packages", packageRoute);
 router.use("/users", userRoute);
 router.use("/services", serviceRoute);
+router.use("/providers", providerRoute);
 
 export default router;
